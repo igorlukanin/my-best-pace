333cf3e09f641a2578c59c121386c05b512321fc
Igor Lukanin <mail@igor.lukanin.name>
2015-12-08 08:05:03 +0500

    Using Strava API to authenticate and list user's activities

--- website/src/app.js
+++ website/src/app.js
@@ -2,6 +2,7 @@ var compression = require('compression'),
     config      = require('config'),
     ejs         = require('ejs'),
     express     = require('express'),
+    strava      = require('strava-v3'),
 
     port        = config.get('server.port');
 
@@ -16,6 +17,32 @@ express()
 
     .get('/', function(req, res) {
         res.render('index');
+    })
+
+    .get('/auth/strava', function(req, res) {
+        res.redirect(strava.oauth.getRequestAccessURL({
+            scope:         'public'
+        }));
+    })
+
+    .get('/auth/strava/complete', function(req, res) {
+        var code = req.query.code;
+
+        if (code) {
+            strava.oauth.getToken(code, function(err, result) {
+                strava.athlete.listActivities({
+                    'access_token': result.access_token
+                }, function(err, result2) {
+                    res.render('index', {
+                        user:       result.athlete,
+                        activities: result2
+                    });
+                });
+            });
+        }
+        else {
+            res.render('index'); // TODO: Change to more meaningful solution
+        }
     })
 
     .set('view engine', 'ejs')
