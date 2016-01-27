517236acecd62f2275d57e3d947bdd3c2254957d
Igor Lukanin <mail@igor.lukanin.name>
2016-01-27 12:37:18 +0500

    New app replenisher-ctrl to restart the replenisher app every X minutes

---
+++ src/replenisher-ctrl.js
@@ -0,0 +1,32 @@
+var childProcess      = require('child_process'),
+    config            = require('config'),
+
+    log               = require('./util/log'),
+
+    restartIntervalMs = config.get('replenisher-ctrl.restart_interval_ms');
+
+
+process.on('uncaughtException', function(err) {
+    console.error(err.stack);
+});
+
+var children = [];
+
+var restartChild = function() {
+    // Start new child
+    children.push(childProcess.fork(__dirname + '/replenisher'));
+
+    log.appState('replenisher-ctrl', 'child spawned, now ' + children.length + ' child(ren)');
+
+    // Stop old child
+    if (children.length > 1) {
+        children.shift().kill();
+
+        log.appState('replenisher-ctrl', 'child stopped, now ' + children.length + ' child');
+    }
+};
+
+restartChild();
+setInterval(restartChild, restartIntervalMs);
+
+log.appState('replenisher-ctrl', 'started');
