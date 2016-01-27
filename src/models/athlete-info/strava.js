f1373c7a8456e0666b7f0357a60bff73fb0bcf02
Igor Lukanin <mail@igor.lukanin.name>
2016-01-27 14:12:22 +0500

    Splitting activities into distance groups

--- src/models/athlete-info/strava.js
+++ src/models/athlete-info/strava.js
@@ -4,10 +4,19 @@ var extractAthleteData = function(athlete) {
     };
 };
 
+// Strava excludes resting time from moving_time, that's why elapsed_time is used
+// See http://blog.strava.com/run-activity-page-updates-and-improvements-6500/
 var extractActivitiesData = function(activities) {
     return activities.filter(function(activity) {
         return activity.data.type == 'Run'
             && activity.data.manual == false;
+    }).map(function(activity) {
+        return {
+            id:          activity.id,
+            distance_km: activity.data.distance / 1000,
+            time_m:      activity.data.elapsed_time / 60,
+            date:        activity.data.start_date
+        };
     });
 };
 
