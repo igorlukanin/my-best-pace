8845238c430bd94d9bf4877ae0e09e0cbc4e75c3
Igor Lukanin <mail@igor.lukanin.name>
2016-01-28 12:15:26 +0500

    Changed 'ejs' views template engine to 'ect'. Added d3.js scripts

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,10 @@
 var router   = require('express').Router();
 
 
+router.get('/', function(req, res) {
+    res.render('index');
+});
+
 router.use('/login',    require('./login'));
 router.use('/athletes', require('./athlete'));
 
--- src/website.js
+++ src/website.js
@@ -1,6 +1,6 @@
 var compression = require('compression'),
     config      = require('config'),
-    ejs         = require('ejs'),
+    ect         = require('ect'),
     express     = require('express'),
 
     controllers = require('./controllers'),
@@ -13,92 +13,18 @@ process.on('uncaughtException', function(err) {
     console.error(err.stack);
 });
 
-//var formatResult = function(effort) {
-//    var distance = (effort.activity.distance / 1000).toFixed(1),
-//        time     = moment.duration(effort.activity.time, "seconds").format("h:mm:ss"),
-//        pace     = moment.duration(effort.time / effort.distance * 1000, "seconds").format("h:mm:ss");
-//
-//    return pace + ' (' + distance + 'km, ' + time + ')';
-//};
-
 express()
-    .use(express.static('public'))
+    .use('/static', express.static('static'))
+    .use('/static/js/d3', express.static('node_modules/d3'))
+
     .use(compression())
     .use(controllers)
 
-    .get('/', function(req, res) {
-        res.render('index');
-    })
-
-//    .get('/auth/strava/complete', function(req, res) {
-//        var code = req.query.code;
-//
-//        if (code) {
-//            strava.oauth.getToken(code, function(err, result) {
-//                console.log(result);
-//
-//                strava.athlete.listActivities({
-//                    access_token: result.access_token,
-//                    per_page: 200
-//                }, function(err, activitiez) {
-//                    var efforts = {};
-//console.log(activitiez.length);
-//                    activities.map(function(activity) {
-//                        return activity.best_efforts.map(function(effort) {
-//                            if (efforts[effort.name] == undefined) {
-//                                efforts[effort.name] = [];
-//                            }
-//
-//                            efforts[effort.name].push({
-//                                time:     effort.moving_time,
-//                                distance: effort.distance,
-//                                activity: {
-//                                    id:       activity.id,
-//                                    time:     activity.moving_time,
-//                                    distance: activity.distance
-//                                }
-//                            });
-//                        });
-//                    });
-//
-//                    for (var name in efforts) {
-//                        if (efforts.hasOwnProperty(name)) {
-//                            var effort = efforts[name].reduce(function(prev, curr) {
-//                                return prev.moving_time < curr.moving_time ? prev : curr;
-//                            });
-//
-//                            efforts[name] = formatResult(effort);
-//                        }
-//                    }
-//
-//                    res.render('index', {
-//                        user:       result.athlete,
-//                        efforts:    efforts
-//                    });
-//
-////                    async.series(activities.map(function(activity) {
-////                        return function(callback) {
-////                            strava.activities.get({
-////                                id: activity.id
-////                            }, function(err, activity) {
-////                                callback(null, activity);
-////                            });
-////                        };
-////                    }), function(err, detailedActivities) {
-////                        res.render('index', {
-////                            user:       result.athlete,
-////                            activities: detailedActivities
-////                        });
-////                    });
-//                });
-//            });
-//        }
-//        else {
-//            res.render('index'); // TODO: Change to more meaningful solution
-//        }
-//    })
-
-    .set('view engine', 'ejs')
+    .set('view engine', 'ect')
+    .engine('ect', ect({
+        watch: true,
+        root: __dirname + '/../views'
+    }).render)
 
     .listen(port, function() {
         log.appState('website', 'started at port ' + port);
