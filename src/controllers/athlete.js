76ee38475602f02e5c6263266a39b2bd33bd9daa
Igor Lukanin <mail@igor.lukanin.name>
2016-01-28 13:00:51 +0500

    Basic pace to distance graph

--- src/controllers/athlete.js
+++ src/controllers/athlete.js
@@ -6,12 +6,29 @@ var router   = require('express').Router(),
 router.get('/:id', function(req, res) {
     var id = req.params.id;
 
+    // TODO: Check if all athleteInfo data is needed to server request
     athletes.getInfo(id, function(err, athleteInfo) {
         if (err) {
             res.render('errors/athlete');
         }
         else {
             res.render('athlete', athleteInfo);
+        }
+    });
+});
+
+router.get('/:id/activities.json', function(req, res) {
+    var id = req.params.id;
+
+    // TODO: Check if all data athleteInfo is needed to server request
+    athletes.getInfo(id, function(err, athleteInfo) {
+        if (err) {
+            res.render({
+                message: 'Error'
+            });
+        }
+        else {
+            res.json(athleteInfo.activities);
         }
     });
 });
