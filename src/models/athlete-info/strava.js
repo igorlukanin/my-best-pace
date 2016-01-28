0b1e386850a13274f685ed79016dce471d24c4d3
Igor Lukanin <mail@igor.lukanin.name>
2016-01-28 13:11:56 +0500

    Basic pace to time graph

--- src/models/athlete-info/strava.js
+++ src/models/athlete-info/strava.js
@@ -1,10 +1,13 @@
+var moment = require('moment');
+
+
 var extractAthleteData = function(athlete) {
     return {
         id: athlete.id
     };
 };
 
-// Strava excludes resting time from moving_time, that's why elapsed_time is used
+// Strava excludes resting time from moving_time so it's more accurate
 // See http://blog.strava.com/run-activity-page-updates-and-improvements-6500/
 var extractActivitiesData = function(activities) {
     return activities.filter(function(activity) {
@@ -14,11 +17,12 @@ var extractActivitiesData = function(activities) {
         return {
             id:          activity.id,
             distance_km: activity.data.distance / 1000,
-            time_m:      activity.data.elapsed_time / 60,
-            date:        activity.data.start_date
+            time_m:      activity.data.moving_time / 60,
+            date:        moment(activity.data.start_date).unix()
         };
     });
 };
+
 
 module.exports = {
     extractAthleteData:    extractAthleteData,
