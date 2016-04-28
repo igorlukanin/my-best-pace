654d8c60b30d38ac7bcf9174510376af3854a3a5
Igor Lukanin <mail@igor.lukanin.name>
2016-04-28 07:50:25 +0500

    Random athletes on landing page

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,9 +4,15 @@ var router = require('express').Router(),
 
 
 router.get('/', (req, res) => db.c.then((c) => db.athletes
+    .limit(10)
     .run(c)
     .then((cursor) => cursor.toArray())
-    .then((athleteInfos) => res.render('index', { athletes: athleteInfos }))));
+    .then((athletes) => res.render('index', {
+        athletes: athletes.map((athlete) => ({
+            name: athlete.full_name,
+            url: '/athletes/' + athlete.id
+        }))}
+    ))));
 
 router.use('/login',    require('./login'));
 
